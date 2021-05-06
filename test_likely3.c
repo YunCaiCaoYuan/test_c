af0ffec9a3b57676bee2e69b0fd895fa54e4be90
sunbin <769460962@qq.com>
2021-05-06 11:33:01 +0800

    test likely

---
+++ test_likely3.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+int main(int argc, char *argv[])
+{
+       int i = atoi(argv[1]); /* init i with the value that GCC can't optimize */
+
+         if (unlikely(i > 0)){
+                 i--;
+         }else{
+                 i++;
+                 }
+
+         return i;
+ }
