ff9ddf2b59a2bad8385e6dd7e84b7fc40724037b
sunbin <>
2022-01-19 17:52:01 +0800

    debug

--- io_reuse/multi_port/multi_port.cpp
+++ io_reuse/multi_port/multi_port.cpp
@@ -102,6 +102,7 @@ int main(int argc, char *argv[]) {
                 ret = recvfrom(udpfd, buf, UDP_BUFFER_SIZE - 1, 0, (struct sockaddr *) &client_address,
                                &client_addrlength);
                 if (ret > 0) {
+                    printf("udp recv ok and send\n");
                     sendto(udpfd, buf, UDP_BUFFER_SIZE - 1, 0, (struct sockaddr *) &client_address, client_addrlength);
                 }
             } else if (events[i].events & EPOLLIN) {
