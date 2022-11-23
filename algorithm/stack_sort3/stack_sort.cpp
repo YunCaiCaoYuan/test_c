fa2cfa8c86442392cb5c8a8f30f956dd5aab3e9f
sunbin <yourName@gmail.com>
2022-11-23 13:10:06 +0800

    研读大顶堆

--- algorithm/stack_sort3/stack_sort.cpp
+++ algorithm/stack_sort3/stack_sort.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+void printArr(vector<int> &nums) {
+    cout << "printArr:";
+    for (auto const &x: nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
 // 构建最小堆
 void FF(vector<int> &nums, int start, int end) {
     int i = start, j = 2 * i + 1;
@@ -25,6 +33,7 @@ void SortFF(vector<int> &nums, int n) {
     while (pos >= 0) {//从下往上局部，最后整体去调整成小顶堆
         FF(nums, pos, n - 1);
         pos -= 1;
+        printArr(nums);
     }
     //调整完了此时这个二叉堆最上面就是最小的数
     //在数组里就是一顿调整之后，第一个元素是最小的
@@ -33,6 +42,7 @@ void SortFF(vector<int> &nums, int n) {
         swap(nums[0], nums[pos]);
         pos -= 1;
         FF(nums, 0, pos);
+//        printArr(nums);
     }
 }
 // 6 2 1 4
@@ -41,6 +51,7 @@ int main() {
 //    int a[]={53, 17, 78, 9, 45, 65, 87, 23};
     int a[]={6, 2, 1, 4};
     vector<int> vv(a,a+sizeof(a)/sizeof(a[0]));
+    printArr(vv);
 
     SortFF(vv, vv.size());
     for (auto const &x: vv) {
@@ -48,5 +59,7 @@ int main() {
     }
     return 0;
 }
+
+
 
 // 降序：87 78 65 53 45 23 17 9
